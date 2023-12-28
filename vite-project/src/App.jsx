0484da34be922608fd292edccb22e4add207b323
judek <kiguthadeus@gmail.com>
2023-12-28 15:44:45 -0500

    changes to app

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Note from "./components/Note";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import notes from "./notes.js";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Header />
-      <Note />
+      {notes.map((jote) => (
+        <Note key={jote.id} title={jote.title} content={jote.content} />
+      ))}
       <Footer />
     </>
   );
--- vite-project/src/components/Note.jsx
+++ vite-project/src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Note() {
+export default function Note(props) {
   return (
     <div className="note">
-      <h1>This is the note Title</h1>
-      <p>This is the Content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
